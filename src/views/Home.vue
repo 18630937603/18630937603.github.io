892d99f3313b08c9a585a527e264f30dd06a46b6
lzh <18630937603@163.com>
2021-11-04 11:03:38 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="changeColor()">changeColor</button>
+    <button @click="getGeoLocation()">获取地理位置</button>
+    <div>地理位置：</div>
+    <div>经度：{{ state.latitude }}</div>
+    <div>纬度：{{ state.longitude }}</div>
+    <div>错误：{{ state.errorMsg }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, reactive } from 'vue';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
+  setup() {
+    const state = reactive({
+      latitude: '',
+      longitude: '',
+      errorMsg: ''
+    })
+    function getGeoLocation() {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        state.latitude = '' + position.coords.latitude;
+        state.longitude = '' + position.coords.longitude;
+      },function (err){
+        state.errorMsg = '' + err
+      });
+    }
+    return {
+      state,
+      getGeoLocation
+    }
+  },
+  methods: {
+    changeColor() {
+      document?.querySelector('meta[name="theme-color"]')?.setAttribute('content','#FF0000')
+    }
   },
 });
 </script>
