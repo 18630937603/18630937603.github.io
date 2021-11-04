f0b7cbd40887acb379e8f73af92b8040d08534f5
lzh <18630937603@163.com>
2021-11-04 11:05:03 +0800

    initial commit

---
+++ docs/js/precache-manifest.3dd263756e31e2e444ea09c1831f86f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfa39b9279aea5b3b3b",
+    "url": "/css/app.b0aefef5.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a4a0f2d9ada321c4a9228dbd83d8075b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bd7d9b0f35a4368c44c",
+    "url": "/js/about.278c3bc0.js"
+  },
+  {
+    "revision": "cdfa39b9279aea5b3b3b",
+    "url": "/js/app.491081fa.js"
+  },
+  {
+    "revision": "8285c4910667880b954b",
+    "url": "/js/chunk-vendors.abd45c19.js"
+  },
+  {
+    "revision": "5d45d0a12d57c10997f3c010600cdbb6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
