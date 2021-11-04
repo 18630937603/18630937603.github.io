6c45d0f3faf0095048346a1ae8e110c132be66e3
lzh <18630937603@163.com>
2021-11-04 13:46:31 +0800

    init2

---
+++ docs/js/precache-manifest.f3f1838dee42cf9aa267946954cc649b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50598b49315bb6ef548c",
+    "url": "/css/app.b0aefef5.css"
+  },
+  {
+    "revision": "b71eefe7ebcb45754279fcf1b725eebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bd7d9b0f35a4368c44c",
+    "url": "/js/about.278c3bc0.js"
+  },
+  {
+    "revision": "50598b49315bb6ef548c",
+    "url": "/js/app.6caccc47.js"
+  },
+  {
+    "revision": "8285c4910667880b954b",
+    "url": "/js/chunk-vendors.abd45c19.js"
+  },
+  {
+    "revision": "5d45d0a12d57c10997f3c010600cdbb6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
